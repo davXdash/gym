6dcb1fa0ca4fdc656eb9d7027c941d08129bcd7d
davXdash <153227675+davXdash@users.noreply.github.com>
2026-08-03 06:43:47 +0200

    Cache two-decimal weight display patch

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE='gym-shell-v18';
-const ASSETS=['./','./index.html','./css/app-v11.css','./css/patch-v13.css','./css/patch-v14.css','./css/training-v18.css','./js/app-v11.js','./js/app-fixes-v15.js','./js/training-v18.js','./js/supabase-config.js','./manifest.webmanifest','./IMG_3040.png','./IMG_3041.png','./IMG_3042.png','./IMG_3043.png','./IMG_3044.png','./IMG_3045.png','./IMG_3046.png','./IMG_3047.png','./IMG_3048.png','./IMG_3049.png','./IMG_3050.png','./IMG_3051.png','./IMG_3062.png','./IMG_3063.png','./IMG_3064.png','./IMG_3065.png','./IMG_3066.png','./IMG_3067.png','./IMG_3068.png','./IMG_3069.png','./IMG_3070.png','./IMG_3071.png'];
+const CACHE='gym-shell-v18b';
+const ASSETS=['./','./index.html','./css/app-v11.css','./css/patch-v13.css','./css/patch-v14.css','./css/training-v18.css','./js/app-v11.js','./js/app-fixes-v15.js','./js/training-v18.js','./js/weight-precision-v18.js','./js/supabase-config.js','./manifest.webmanifest','./IMG_3040.png','./IMG_3041.png','./IMG_3042.png','./IMG_3043.png','./IMG_3044.png','./IMG_3045.png','./IMG_3046.png','./IMG_3047.png','./IMG_3048.png','./IMG_3049.png','./IMG_3050.png','./IMG_3051.png','./IMG_3062.png','./IMG_3063.png','./IMG_3064.png','./IMG_3065.png','./IMG_3066.png','./IMG_3067.png','./IMG_3068.png','./IMG_3069.png','./IMG_3070.png','./IMG_3071.png'];
 const prepareHtml=async response=>{
  const type=response.headers.get('content-type')||'';
  if(!type.includes('text/html'))return response;
@@ -11,6 +11,7 @@ const prepareHtml=async response=>{
  if(!html.includes('training-v18.css'))html=html.replace('</head>','<link rel="stylesheet" href="css/training-v18.css"></head>');
  if(!html.includes('app-fixes-v15.js'))html=html.replace('</body>','<script type="module" src="js/app-fixes-v15.js"></script></body>');
  if(!html.includes('training-v18.js'))html=html.replace('</body>','<script type="module" src="js/training-v18.js"></script></body>');
+ if(!html.includes('weight-precision-v18.js'))html=html.replace('</body>','<script src="js/weight-precision-v18.js"></script></body>');
  return new Response(html,{status:response.status,statusText:response.statusText,headers:{...Object.fromEntries(response.headers.entries()),'content-type':'text/html; charset=utf-8'}});
 };
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)));self.skipWaiting()});
