536a851542d6d008cbdfb1e50d6d574bfc9b86c7
davXdash <153227675+davXdash@users.noreply.github.com>
2026-08-05 09:14:48 +0200

    Activate progressive overload coach

--- js/status-fix-v25.js
+++ js/status-fix-v25.js
@@ -1,4 +1,5 @@
-import './app-v55.js?v=55';
-import './app-v53.js?v=55';
-import './app-v54.js?v=55';
+import './app-v55.js?v=57';
+import './app-v53.js?v=57';
+import './app-v54.js?v=57';
+import './coach-progressive-v57.js?v=57';
 export {};
